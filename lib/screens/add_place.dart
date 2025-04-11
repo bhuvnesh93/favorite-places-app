f6cd12cafcc125fd64ddbe0e62519125134b2ec5
Bhuvneshwar Garg <garg.bhuvnesh93@gmail.com>
2025-04-11 23:25:06 +0530

    first commit

---
+++ lib/screens/add_place.dart
@@ -0,0 +1,72 @@
+import 'dart:io';
+
+import 'package:favorite_places/models/place.dart';
+import 'package:favorite_places/providers/user_places.dart';
+import 'package:favorite_places/widgets/image_input.dart';
+import 'package:favorite_places/widgets/location_input.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AddPlaceScreen extends ConsumerStatefulWidget {
+  const AddPlaceScreen({super.key});
+
+  @override
+  ConsumerState<AddPlaceScreen> createState() {
+    return _AddPlaceScreenState();
+  }
+}
+
+class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+  File? _selectedImage;
+  PlaceLocation? _selectedLocation;
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  void _savePlace() {
+    final enteredTitle = _titleController.text;
+    if (enteredTitle.isEmpty || _selectedImage == null) {
+      return;
+    }
+
+    ref
+        .read(userPlacesProvider.notifier)
+        .addPlace(enteredTitle, _selectedImage!, _selectedLocation!);
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Add new place")),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: "Title"),
+              controller: _titleController,
+              style: TextStyle(color: Theme.of(context).colorScheme.onSurface),
+            ),
+            SizedBox(height: 10),
+            ImageInput(onPickImage: (image) => _selectedImage = image),
+            SizedBox(height: 16),
+            LocationInput(
+              onSelectLocation: (location) => _selectedLocation = location,
+            ),
+            SizedBox(height: 16),
+            ElevatedButton.icon(
+              onPressed: _savePlace,
+              label: Text("Add Place"),
+              icon: Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
